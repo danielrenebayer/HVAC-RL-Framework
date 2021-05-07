35653d7b7579a1ef0c1979a4c5a3aa09ce650a9e
Daniel Rene Bayer <daniel.bayer@fau.de>
2021-05-07 14:37:36 +0200

    New Network: 2HiddenLayer,Trapezium,SiLU

--- Networks.py
+++ Networks.py
@@ -18,6 +18,17 @@ def generate_network(network_type, input_size, output_size):
                 torch.nn.Linear(hidden_size2, output_size)
             )
 
+    if network_type == "2HiddenLayer,Trapezium,SiLU":
+        hidden_size1 = max(19, input_size)
+        hidden_size2 = (hidden_size1 + 2*output_size) // 3
+        return torch.nn.Sequential(
+                torch.nn.Linear(input_size,   hidden_size1),
+                torch.nn.SiLU(),
+                torch.nn.Linear(hidden_size1, hidden_size2),
+                torch.nn.SiLU(),
+                torch.nn.Linear(hidden_size2, output_size)
+            )
+
     if network_type == "1HiddenBigLayer":
         hidden_size1 = 3 * max(input_size, output_size)
         return torch.nn.Sequential(
